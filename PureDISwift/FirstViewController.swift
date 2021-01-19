306c08dce965068ecb8a41f52c96e2c1b6d4a101
shin donggyu <donggyu9410@gmail.com>
2021-01-19 14:08:25 +0900

    [Release] 보일러플레이트 코드

--- PureDISwift/FirstViewController.swift
+++ PureDISwift/FirstViewController.swift
@@ -10,7 +10,19 @@ import UIKit
 class FirstViewController: UIViewController {
     
     // MARK: Properties
+    private let stringGenerator:StringGenerator = StringGenerator()
     
+    private lazy var stringLabel:UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private lazy var redirectButton:UIButton = {
+        let bt = UIButton(type: UIButton.ButtonType.system)
+        bt.setTitle("두번째 뷰", for: UIControl.State.normal)
+        bt.addTarget(self, action: #selector(redirectButtonTapped), for: UIControl.Event.touchUpInside)
+        return bt
+    }()
 
     // MARK: Lifecycles
     override func viewDidLoad() {
@@ -18,12 +30,36 @@ class FirstViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         configureUI()
+        drawView()
     }
     
     // MARK: Helpers
-    func configureUI() {
-        view.backgroundColor = .systemBackground
+    func drawView() {
+        self.stringLabel.text = stringGenerator.generate()
     }
     
+    func configureUI() {
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(stringLabel)
+        stringLabel.translatesAutoresizingMaskIntoConstraints = false
+        stringLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stringLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        
+        view.addSubview(redirectButton)
+        redirectButton.translatesAutoresizingMaskIntoConstraints = false
+        redirectButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        redirectButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+    }
+    
+    func presentSecondViewController() {
+        let secondView = SecondViewController()
+        self.present(secondView, animated: true, completion: nil)
+    }
+    
+    // MARK: Selectors
+    @objc func redirectButtonTapped() {
+        presentSecondViewController()
+    }
 
 }
--- PureDISwift/SecondViewController.swift
+++ PureDISwift/SecondViewController.swift
@@ -9,17 +9,34 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    // MARK: Properties
+    private let stringGenerator:StringGenerator = StringGenerator()
+    
+    private lazy var stringLabel:UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
     // MARK: Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         configureUI()
+        drawView()
     }
     
     // MARK: Helpers
+    func drawView() {
+        self.stringLabel.text = stringGenerator.generate()
+    }
+    
     func configureUI() {
         view.backgroundColor = .systemBackground
+        view.addSubview(stringLabel)
+        stringLabel.translatesAutoresizingMaskIntoConstraints = false
+        stringLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stringLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
     }
 
 }
